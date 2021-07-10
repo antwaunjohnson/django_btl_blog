c62814369cf7b79b15a4a8e7f716f27fba2eda39
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2021-07-09 23:19:34 -0700

    Added urlpatterns to blogs app and tested http request methods

---
+++ btl_blogs/blogs/api.py
@@ -0,0 +1,14 @@
+from blogs.models import Blog
+from rest_framework import viewsets, permissions
+from .serializers import BlogSerializer
+
+# Blog Viewset
+
+
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Blog.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+    serializer_class = BlogSerializer
