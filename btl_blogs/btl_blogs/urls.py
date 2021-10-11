ae23beaac1bdbe36f8e7b8eb1bdb39e8ebf22496
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2021-10-10 19:27:00 -0700

    deleted route for frontend django app

--- btl_blogs/btl_blogs/urls.py
+++ btl_blogs/btl_blogs/urls.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('frontend.urls')),
     path('api/', include('blogs.urls')),
     path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
-
 ]
