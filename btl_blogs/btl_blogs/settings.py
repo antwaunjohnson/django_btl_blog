7cbc78cad3b607b161ad878ff832cb562b035d8e
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2021-11-07 15:59:57 -0700

    updated project get random secret key

--- btl_blogs/btl_blogs/settings.py
+++ btl_blogs/btl_blogs/settings.py
@@ -1,5 +1,7 @@
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,14 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOST') or None
-if not DEBUG:
-    ALLOWED_HOSTS += [config('DJANGO_ALLOWED_HOST')]
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOST', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
